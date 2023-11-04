17c23c785e3fe299c7f805d898f4e9bacc06944c
DussanSM <smarind_1@uqvirtual.edu.co>
2023-11-04 11:28:29 -0500

    inicio

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.proyectoprocesos {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.proyectoprocesos to javafx.fxml;
+    exports com.example.proyectoprocesos;
+}
